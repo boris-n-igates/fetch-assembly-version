c5888c8f54d6068bdbf03949a794746c1059f035
boris-n-igates <84454529+boris-n-igates@users.noreply.github.com>
2022-11-27 13:42:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try{
+    console.log(`Hello jdhjkshdjkdhfkjhd`);
     core.setOutput("assembly-version", "123456789aaaa");
 }catch (error){
     core.setFailed(error.message);
