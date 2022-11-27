d7612f5ccc4f35f945d5d5e7c6408e1dabe3add7
Boris Nadysev <boris@igates.co.il>
2022-11-27 13:40:26 +0200

    test

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+
+try{
+    core.setOutput("assembly-version", "123456789aaaa");
+}catch (error){
+    core.setFailed(error.message);
+}
