f959e50829fca75e0880917c3009593fe0361d59
Boris Nadysev <boris@igates.co.il>
2022-11-27 15:31:55 +0200

    input path

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
+
 
 try{
     console.log(`Hello jdhjkshdjkdhfkjhd`);
     core.setOutput("assembly-version", "123456789aaaa");
+    const rootPath = core.getInput("project-path", { required: true })
+    fs.readdir(rootPath, function(error, files){
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        const ind = files.findIndex((value, index) => {
+            return value.includes('csproj');
+        }) 
+
+        console.log('--------- ' + files[ind]);
+    })
+
 }catch (error){
     core.setFailed(error.message);
 }
